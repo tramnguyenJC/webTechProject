a296b140d613f9c44465b6ecf9a3f0f3e03a59f6
Tracy Nguyen <tracy.nguyen@richmond.edu>
2019-02-24 17:13:01 +0000

    Front page styling

--- site/database.js
+++ site/database.js
@@ -1,8 +1,8 @@
 "use strict";
-
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('data.db');
 
+// Create the database.
 exports.createDatabase = function() {
   db.serialize(() => {
   // create a new database table:
@@ -18,7 +18,11 @@ exports.createDatabase = function() {
   });
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// PRODUCTS METHODS
 
+// Insert product into the database
+// @param product: dict object that contains information about the product.
 exports.insertProduct = function(product) {
   db.serialize(() => {
     var command = "INSERT INTO Products (id, name, category, price, quantity," +
@@ -30,6 +34,9 @@ exports.insertProduct = function(product) {
   });
 }
 
+// Retrieve product in database by product id
+// @param productId: given productId
+// @param callback: matching product to be returned
 exports.getProductById = function(productId, callback) {
   db.serialize(() => {
     // db.all() fetches all results from an SQL query into the 'rows' variable:
@@ -50,3 +57,25 @@ exports.getProductById = function(productId, callback) {
   });
 }
 
+// Retrieve product in database by product name
+// @param productName: given product name
+// @param callback: matching product to be returned
+exports.getProductByName = function(productName, callback) {
+  db.serialize(() => {
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all(
+      'SELECT * FROM Products WHERE name=$name',
+      {
+        $name: Number(productName)  
+      },
+      // callback function to run when the query finishes:
+      (err, rows) => {
+        if (rows.length != 0) {
+          callback(rows[0]);
+        } else {
+          callback(null);
+        }
+      }
+    );
+  });
+}
--- site/routes/products.js
+++ site/routes/products.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var database = require('../database.js')
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('data.db');
 
 router.get('/', function(req, res, next) {
   res.send('All products goes here');
@@ -12,6 +10,7 @@ router.get('/:productid', (req, res) => {
   const productId = Number(req.params.productid); // matches ':productid' above
   
   database.getProductById(productId, function(product) {
+    // If the retrieved product is not null (the product Id exists).
     if (product) {
       res.render('product', { 
       name: product['name'],
