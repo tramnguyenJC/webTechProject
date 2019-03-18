c3ef4164c29d9f1267d4a4e937c8d80800d44ed8
Tracy Nguyen <tracy.nguyen@richmond.edu>
2019-03-18 02:07:16 +0000

    Changed some styles

---
+++ site/public/javascripts/header.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 200) {
+			$(".navbar").addClass("navbar-scrolled");
+			$(".navbar a").addClass("navbar-scrolled");
+		}
+		else {
+			$(".navbar").removeClass("navbar-scrolled");
+			$(".navbar a").removeClass("navbar-scrolled");
+		}
+
+		if ($(window).scrollTop() > 200) {
+			$(".navbar").addClass("navbar-scrolled");
+			$(".navbar a").addClass("navbar-scrolled");	
+		}
+	});
+});
+
