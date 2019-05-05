9602a10d3b5b7433d80651dee2ba61ac3af5feaa
Tracy Nguyen <tracy.nguyen@richmond.edu>
2019-05-05 23:12:28 +0100

    Fixed

--- site/database.js
+++ site/database.js
@@ -15,37 +15,10 @@ exports.createDatabase = function() {
     
     // Create Users table
     db.run("CREATE TABLE IF NOT EXISTS Users (" +
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       "username VARCHAR(255) PRIMARY KEY NOT NULL, " +
       "password VARCHAR(255) NOT NULL, " +
       "isAdmin BOOLEAN)");
-=======
-      "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-      "username VARCHAR(255) NOT NULL UNIQUE, " +
-      "password VARCHAR(255) NOT NULL)");
->>>>>>> parent of aff6c2e... Stored username and password in .env
-=======
-      "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-      "username VARCHAR(255) NOT NULL UNIQUE, " +
-      "password VARCHAR(255) NOT NULL)");
->>>>>>> parent of aff6c2e... Stored username and password in .env
 
-    db.run("CREATE TABLE IF NOT EXISTS ShoppingCart (" +
-      "userid INTEGER NOT NULL, " +
-      "productid INTEGER NOT NULL, " +
-      "quantity INTEGER NOT NULL, " + 
-      "FOREIGN KEY(productid) REFERENCES Products(id), " +
-      "FOREIGN KEY(userid) REFERENCES Users(id), " + 
-      "PRIMARY KEY (userid, productid) )");  
-=======
-      "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-      "username VARCHAR(255) NOT NULL UNIQUE, " +
-      "password VARCHAR(255) NOT NULL)");
->>>>>>> parent of aff6c2e... Stored username and password in .env
-
-    console.log('successfully created the tables in data.db');
   });
 }
 
